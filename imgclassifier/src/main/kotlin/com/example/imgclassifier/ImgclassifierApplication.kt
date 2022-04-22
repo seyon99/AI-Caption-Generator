ed1a66ac47ffa00652a9bafa16fa12834a0c5689
seyon99 <seyonkuganesan@gmail.com>
2022-04-21 20:55:54 -0400

    init boilerplate

---
+++ imgclassifier/src/main/kotlin/com/example/imgclassifier/ImgclassifierApplication.kt
@@ -0,0 +1,11 @@
+package com.example.imgclassifier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImgclassifierApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImgclassifierApplication>(*args)
+}
---
+++ imgclassifier/src/test/kotlin/com/example/imgclassifier/ImgclassifierApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.imgclassifier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImgclassifierApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
