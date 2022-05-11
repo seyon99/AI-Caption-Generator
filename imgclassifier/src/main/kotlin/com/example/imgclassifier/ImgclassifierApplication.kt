236a13467a61b0d34610c29f9342315751054a58
seyon99 <seyonkuganesan@gmail.com>
2022-05-11 08:29:04 -0400

    added py model for generating captions

--- imgclassifier/src/main/kotlin/com/example/imgclassifier/ImgclassifierApplication.kt
+++ imgclassifier/src/main/kotlin/com/example/imgclassifier/ImgclassifierApplication.kt
@@ -2,6 +2,9 @@ package com.example.imgclassifier
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class ImgclassifierApplication
@@ -9,3 +12,16 @@ class ImgclassifierApplication
 fun main(args: Array<String>) {
 	runApplication<ImgclassifierApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+			Message("1", "Sending Image 1"),
+			Message("2", "Sending Image 2"),
+			Message("3", "Sending Image 3"),
+	)
+}
+
+// https://kotlinlang.org/docs/jvm-spring-boot-restful.html#run-the-application
+data class Message(val id: String?, val text: String)
