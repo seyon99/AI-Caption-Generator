28270ab2c622a2fc4245a78e687115f97ab5246b
seyon99 <seyonkuganesan@gmail.com>
2022-05-12 15:35:00 -0400

    refactored s3 upload endpoint

--- imgclassifier/src/main/kotlin/com/example/imgclassifier/ImgclassifierApplication.kt
+++ imgclassifier/src/main/kotlin/com/example/imgclassifier/ImgclassifierApplication.kt
@@ -4,6 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import java.sql.Blob
 import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.model.PutObjectRequest
@@ -20,38 +26,33 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+@RequestMapping("/captiongen")
+class CaptionGenerationController() {
 	@GetMapping
-	fun index(): List<Message> = listOf(
-			Message("1", "Sending Image 1"),
-			Message("2", "Sending Image 2"),
-			Message("3", "Sending Image 3"),
-	)
-}
+	fun findAll() = "{img-link: aws3..., img-caption: A blue dog}"
 
-// https://kotlinlang.org/docs/jvm-spring-boot-restful.html#run-the-application
-data class Message(val id: String?, val text: String)
+	@PostMapping
+	// TODO: implement endpoint to upload image to S3 to trigger lambda fcn.
+	suspend fun putS3Object(bucketName: String, objectKey: String, objectPath: String) {
 
-suspend fun putS3Object(bucketName: String, objectKey: String, objectPath: String) {
+		val metadataVal = mutableMapOf<String, String>()
+		metadataVal["myVal"] = "test"
 
-	val metadataVal = mutableMapOf<String, String>()
-	metadataVal["myVal"] = "test"
+		val request = PutObjectRequest {
+			bucket = bucketName // aikotlin
+			key = objectKey
+			metadata = metadataVal
+			this.body = Paths.get(objectPath).asByteStream()
+		}
 
-	val request = PutObjectRequest {
-		bucket = bucketName
-		key = objectKey
-		metadata = metadataVal
-		this.body = Paths.get(objectPath).asByteStream()
-	}
-
-	S3Client { region = "us-east-1" }.use { s3 ->
-		val response =s3.putObject(request)
-		println("Tag information is ${response.eTag}")
+		S3Client { region = "us-east-2" }.use { s3 ->
+			val response = s3.putObject(request)
+			println("Tag information is ${response.eTag}")
+		}
 	}
 }
 
 
 data class Image(val imgId: String?, val imgObj: Blob)
 
-// TODO: implement endpoint to upload image to S3 to trigger lambda fcn.
 // https://aws.amazon.com/blogs/machine-learning/how-to-deploy-deep-learning-models-with-aws-lambda-and-tensorflow/
